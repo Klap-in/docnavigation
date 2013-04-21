7acecbc59d231549a166d02a681c854d83f74f3a
Klap-in <klapinklapin@gmail.com>
2013-04-21 16:04:44 +0200

    Page navigation first skelet of the plugin

---
+++ action.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * DokuWiki Plugin DocNavigation (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+require_once(DOKU_INC.'inc/parser/xhtml.php');
+
+class action_plugin_docnav extends DokuWiki_Action_Plugin {
+
+    /**
+     * Register the events
+     *
+     * @param Doku_Event_Handler $controller
+     */
+    public function register(Doku_Event_Handler &$controller) {
+        $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, '_addtopnavigation');
+        $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, '_addbottomnavigation');
+    }
+
+    /**
+     * Add navigation bar to top of content
+     *
+     * @param Doku_Event $event
+     * @param            $param
+     */
+    public function _addtopnavigation(Doku_Event &$event, $param) {
+        global $ACT;
+        //if($ACT != 'show') return;
+        if($event->data[0] != 'xhtml' || $ACT != 'show') return;
+
+        $event->data[1] = $this->getNavbar($bottom = false).$event->data[1];
+    }
+
+    /**
+     * Add navigationbar to bottom of content
+     *
+     * @param Doku_Event $event
+     * @param            $param
+     */
+    public function _addbottomnavigation(Doku_Event &$event, $param) {
+        global $ACT;
+        if($ACT != 'show') return;
+
+        //insert before the discussion plugin
+        $pos = strrpos ( $event->data , '<div class="comment_wrapper" id="comment_wrapper">');
+        if($pos) {
+            $event->data = substr_replace($event->data, $this->getNavbar($bottom = true), $pos, 0);
+        } else {
+            $event->data = $event->data.$this->getNavbar($bottom = true);
+        }
+    }
+
+    /**
+     * Return html of navigation elements
+     *
+     * @param bool $bottom add referer to ToC
+     * @return string
+     */
+    private function getNavbar($bottom = true) {
+        global $ID;
+        $data = p_get_metadata($ID, 'docnav');
+
+        $out = '';
+        if(!empty($data)) {
+            $renderer = new Doku_Renderer_xhtml;
+
+            if($bottom) $out .= '<div class="clearer"></div>'.DOKU_LF;
+
+            $out .= '<div class="docnavbar'.($bottom ? ' bottom' : '').'">'.DOKU_LF.DOKU_TAB.'<div class="leftnav">';
+            if($data['previous']) $out .= '← '.$renderer->internallink($data['previous'][0], $data['previous'][1], null, true);
+            $out .= '&nbsp;</div>'.DOKU_LF;
+
+            if($bottom) {
+                $out .= DOKU_TAB.'<div class="centernav">';
+                if($data['toc']) $out .= $renderer->internallink($data['toc'][0], $data['toc'][1], null, true);
+                $out .= '&nbsp;</div>'.DOKU_LF;
+            }
+
+            $out .= DOKU_TAB.'<div class="rightnav">&nbsp;';
+            if($data['next']) $out .= $renderer->internallink($data['next'][0], $data['next'][1], null, true).' →';
+            $out .= '</div>'.DOKU_LF.'</div>'.DOKU_LF;
+        }
+        return $out;
+    }
+}
+
+// vim:ts=4:sw=4:et:
---
+++ syntax/pagenav.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * DokuWiki Plugin DocNavigation (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+class syntax_plugin_docnav_pagenav extends DokuWiki_Syntax_Plugin {
+
+    public function getType() {
+        return 'substition';
+    }
+
+    public function getPType() {
+        return 'block';
+    }
+
+    public function getSort() {
+        return 150;
+    }
+
+    public function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('<-[^\n]*\^[^\n]*\^[^\n]*->', $mode, 'plugin_docnav_pagenav');
+    }
+
+    public function handle($match, $state, $pos, &$handler) {
+        global $conf, $ID;
+
+        // links are: previous, toc, next
+        $links = explode("^", substr($match, 2, -2), 3);
+        foreach($links as &$link) {
+            // Split title from URL
+            $link = explode('|',$link,2);
+            if ( !isset($link[1]) ) {
+                $link[1] = NULL;
+            } else if ( preg_match('/^\{\{[^\}]+\}\}$/',$link[1]) ) {
+                // If the title is an image, convert it to an array containing the image details
+                $link[1] = Doku_Handler_Parse_Media($link[1]);
+            }
+            $link[0] = trim($link[0]);
+        }
+
+        //look for an existing headpage when toc is empty
+        if(!$links[1][0]) {
+            $ns = getNS($ID);
+            if(page_exists($ns.':'.$conf['start'])) {
+                // start page inside namespace
+                $links[1][0] = $ns.':'.$conf['start'];
+            }elseif(page_exists($ns.':'.noNS($ns))) {
+                // page named like the NS inside the NS
+                $links[1][0] = $ns.':'.noNS($ns);
+            }elseif(page_exists($ns)) {
+                // page like namespace exists
+                $links[1][0] = (!getNS($ns) ? ':':'').$ns;
+            }
+        }
+        $data = array(
+            'previous' => $links[0],
+            'toc'      => $links[1],
+            'next'     => $links[2]
+        );
+        return $data;
+    }
+
+    public function render($mode, &$renderer, $data) {
+
+        if($mode == 'metadata') {
+            /** @var Doku_Renderer_metadata $renderer */
+            $renderer->meta['docnav'] = $data;
+
+            foreach($data as $url) {
+                if($url) {
+                    $renderer->internallink($url[0], $url[1]);
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
+
+// vim:ts=4:sw=4:et:
